fbe32cdb7970b1935c2daae5554a168189493a8c
mkrtashot <mkrtchyan.ashot@yahoo.com>
2021-12-17 19:27:20 +0400

    feat: some implementation

---
+++ array_methods/some.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let arr = [1, 2, 3, 4, 5];
+function someFake(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      return true;
+    }
+  }
+  return fake;
+}
+console.log(someFake(arr));
